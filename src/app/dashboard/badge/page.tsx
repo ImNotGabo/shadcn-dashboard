492176eab010924e57b26b64aa061d3b7dedf31c
ImNotGabo <132179691+ImNotGabo@users.noreply.github.com>
2025-01-12 11:17:30 -0400

    add Badge component page to dashboard with various variants

---
+++ src/app/dashboard/badge/page.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import { Badge } from '@/components/ui/badge';
+
+export default function Page() {
+	return (
+		<div className='flex gap-4'>
+			<Badge capitalize>default</Badge>
+			<Badge variant='destructive'>destructive</Badge>
+			<Badge variant='secondary'>secondary</Badge>
+			<Badge variant='outline'>outline</Badge>
+			<Badge
+				capitalize
+				variant='info'
+			>
+				info
+			</Badge>
+			<Badge
+				capitalize
+				variant='success'
+			>
+				success
+			</Badge>
+		</div>
+	);
+}
