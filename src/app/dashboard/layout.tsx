d2ccb5cec8b15eee1f5d2970252632e767f86cdf
ImNotGabo <132179691+ImNotGabo@users.noreply.github.com>
2025-01-12 18:54:51 -0400

    add Avatar component to dashboard layout

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,6 +1,7 @@
 // https://tailwindcomponents.com/component/tailwind-css-admin-dashboard-layout
 // https://gist.github.com/Klerith/3949f1c8b884d7101e378dfb668f0f3a
 
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import Link from 'next/link';
 
 const links = [
@@ -11,6 +12,7 @@ const links = [
 	{ name: 'dialog', href: 'dialog' },
 	{ name: 'badge', href: 'badge' },
 	{ name: 'calendar', href: 'calendar' },
+	{ name: 'avatar', href: 'avatar' },
 ].sort((a, b) => a.name.localeCompare(b.name));
 
 export default function DashboardLayout({
@@ -104,7 +106,13 @@ export default function DashboardLayout({
 						<div className='flex items-center'>
 							{/* User Avatar */}
 							<div className='bg-blue-500 text-white p-2 rounded-full w-12 h-12 flex items-center justify-center'>
-								FH
+								<Avatar>
+									<AvatarImage
+										src='https://avatars.githubusercontent.com/u/132179691?v=4'
+										alt='@ImNotGabo'
+									/>
+									<AvatarFallback>GDR</AvatarFallback>
+								</Avatar>
 							</div>
 						</div>
 					</div>
