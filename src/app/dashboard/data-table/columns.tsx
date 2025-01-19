bd116bad98ad70b4c44897fc98ce803845ae0d99
ImNotGabo <132179691+ImNotGabo@users.noreply.github.com>
2025-01-19 11:31:23 -0400

    enhance data table columns with client name and formatted amount display

--- src/app/dashboard/data-table/columns.tsx
+++ src/app/dashboard/data-table/columns.tsx
@@ -1,12 +1,30 @@
 'use client';
 
 import { Payment } from '@/app/data/payment.data';
+import { Badge } from '@/components/ui/badge';
 import { ColumnDef } from '@tanstack/react-table';
 
 export const columns: ColumnDef<Payment>[] = [
 	{
+		accessorKey: 'clientName',
+		header: 'Client Name',
+	},
+	{
 		accessorKey: 'status',
 		header: 'Status',
+		cell: ({ row }) => {
+			const status = row.getValue('status') as string;
+			const variant =
+				(
+					{
+						pending: 'outline',
+						processing: 'info',
+						success: 'success',
+						failed: 'destructive',
+					} as Record<string, React.ComponentProps<typeof Badge>['variant']>
+				)[status] ?? 'default';
+			return <Badge variant={variant}>{status}</Badge>;
+		},
 	},
 	{
 		accessorKey: 'email',
@@ -14,10 +32,15 @@ export const columns: ColumnDef<Payment>[] = [
 	},
 	{
 		accessorKey: 'amount',
-		header: 'Amount',
-	},
-	{
-		accessorKey: 'clientName',
-		header: 'Client Name',
+		header: () => <div>Amount</div>,
+		cell: ({ row }) => {
+			const amount = parseFloat(row.getValue('amount'));
+			const formatted = new Intl.NumberFormat('en-US', {
+				style: 'currency',
+				currency: 'USD',
+			}).format(amount);
+
+			return <div className='font-medium'>{formatted}</div>;
+		},
 	},
 ];
